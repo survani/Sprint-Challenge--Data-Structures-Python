45c48b1a603f2e00d6ab6175417388c6e11d5c44
survani <giovanicode@outlook.com>
2020-09-25 19:45:01 -0700

    Completed Sprint at 7:45 PM

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode  # imported from my bst file.
 
 start_time = time.time()
 
@@ -13,16 +14,24 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+the_bst = BSTNode("")  # rewuires a positional argument which is value so I decided to use empty string.
+for n1 in names_1:
+    the_bst.insert(n1)
+
+for n2 in names_2:
+    if n2 not in duplicates:
+        if the_bst.contains(n2):
+            duplicates.append(n2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# add this to the above code to see runtime!
+# duplicates = set.intersection(set(names_1), set(names_2))
